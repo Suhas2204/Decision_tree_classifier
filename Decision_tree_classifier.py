49c410ee998323bdd7d64679574cda064deffd14
trydemo <nickjagtap56@gmail.com>
2024-07-24 16:19:41 +0200

    done

--- Decision_tree_classifier.py
+++ Decision_tree_classifier.py
@@ -13,9 +13,9 @@ from sklearn.tree import DecisionTreeClassifier
 # X = music_data.drop(columns='genre')
 # y = music_data['genre']
 # # X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.2)
-
-
-
+#
+#
+#
 # model = DecisionTreeClassifier()
 # model.fit(X,y)
 model = joblib.load('music-recommender.joblib')
@@ -24,6 +24,6 @@ model = joblib.load('music-recommender.joblib')
 
 predictions = model.predict([[21,1]])
 print (predictions)
-#
+
 # score = accuracy_score(y_test,predictions)
 # print('Accuracy score is', score)
